d0e67c70e3f2344a3858373242f000966d81c266
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2023-06-13 16:24:27 +0200

    added new test case to the test_response test file

--- tests/test_basictests.py
+++ tests/test_basictests.py
@@ -3,7 +3,16 @@ import main
 
 # ----------------------test cases ------------------
 def test_multiplication():
-    assert main.multiply(10,4) == 40
+    assert main.multiply(10,4) == 40 
+    assert main.multiply(10000, 25) == 250000 #EDGE CASES testing!!
+    assert main.multiply(-10000, -25) == 250000 #EDGE CASES testing!!
+    assert main.multiply(-10000, 25) == (-10000)*25 #EDGE CASES testing!!
+    
+    # TERMS:
+    # edge case testing- this is when you test the extremes (unlikely use cases) of your function.
+    
+
+    
     
     
 
--- tests/test_response.py
+++ tests/test_response.py
@@ -1,8 +1,18 @@
 import pytest
 import requests
 
+# test 1
+# this test tests the status_code of the internet server get request 
 def test_server_response():
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     assert response.status_code == 200 
+    
+# test 2 
+# 1. this test uses the @pytest.mark.skip() method to skip the test during testing
+# 2. this code checks the length of the internet servers response
+@pytest.mark.skip(reason="Reason for skipping the test") #this line allows you to skip the following test
+def test_length_of_response():
+    response = requests.get('https://jsonplaceholder.typicode.com/posts')
+    assert len(response.json()) != 0 
     
 
